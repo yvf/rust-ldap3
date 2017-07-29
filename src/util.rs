855106ea983d433609f629d53b1405cecca67457
Ivan Nejgebauer <inejge@gmail.com>
2017-07-29 19:21:34 +0200

    Refactor dn_escape()
    * Add logic for escaping only leading ' '/'#' and trailing ' '
    
    * Touch up docs

--- src/util.rs
+++ src/util.rs
@@ -46,23 +46,33 @@ pub fn ldap_escape<'a, S: Into<Cow<'a, str>>>(lit: S) -> Cow<'a, str> {
     }
 }
 
-
-/// Escapes an attribute value in a distinguished name (DN).
+/// Escape an attribute value in a relative distinguished name (RDN).
 ///
-/// For example, a DN might be
-/// `uid=test_user,ou=Users,dc=myldapdomain,dc=myorg,dc=com`, where each of
-/// the fields, or "attribute values" can contain any characters, but some
-/// special characters must be escaped. (space, double quote, number sign,
-/// plus sign, comma, semicolon, less than, greater than and equals signs,
-/// backslash, NUL)
-/// 
-/// If you construct a DN yourself, you have to make sure that the attribute
-/// values are properly escaped.
-pub fn dn_escape<'a, S: Into<Cow<'a, str>>>(lit: S) -> Cow<'a, str> {
+/// When a literal string is used to represent an attribute value in an RDN,
+/// some of it characters might need to be escaped according to the rules
+/// of [RFC 4514](https://tools.ietf.org/html/rfc4514).
+///
+/// The function is named `dn_escape()` instead of `rdn_escape()` because of
+/// a long-standing association of its intended use with the handling of DNs.
+///
+/// The argument, `val`, can be owned or borrowed. The function doesn't
+/// allocate the return value unless there's need to escape the input.
+pub fn dn_escape<'a, S: Into<Cow<'a, str>>>(val: S) -> Cow<'a, str> {
     #[inline]
-    fn needs_escape(c: u8) -> bool {
-        c == b' ' || c == b'"' || c == b'#' || c == b'+' || c == b',' ||
-        c == b';' || c == b'<' || c == b'=' || c == b'>' || c == b'\\' || c == 0
+    fn always_escape(c: u8) -> bool {
+        c == b'"' || c == b'+' || c == b',' || c == b';' ||
+        c == b'<' || c == b'=' || c == b'>' || c == b'\\' ||
+        c == 0
+    }
+
+    #[inline]
+    fn escape_leading(c: u8) -> bool {
+        c == b' ' || c == b'#'
+    }
+
+    #[inline]
+    fn escape_trailing(c: u8) -> bool {
+        c == b' '
     }
 
     #[inline]
@@ -70,13 +80,13 @@ pub fn dn_escape<'a, S: Into<Cow<'a, str>>>(lit: S) -> Cow<'a, str> {
         c + if c < 10 { b'0' } else { b'a' - 10 }
     }
 
-    let lit = lit.into();
+    let val = val.into();
     let mut output = None;
-    for (i, &c) in lit.as_bytes().iter().enumerate() {
-        if needs_escape(c) {
+    for (i, &c) in val.as_bytes().iter().enumerate() {
+        if always_escape(c) || i == 0 && escape_leading(c) || i + 1 == val.len() && escape_trailing(c) {
             if output.is_none() {
-                output = Some(Vec::with_capacity(lit.len() + 12)); // guess: up to 4 escaped chars
-                output.as_mut().unwrap().extend(lit[..i].as_bytes());
+                output = Some(Vec::with_capacity(val.len() + 12)); // guess: up to 4 escaped chars
+                output.as_mut().unwrap().extend(val[..i].as_bytes());
             }
             let output = output.as_mut().unwrap();
             output.push(b'\\');
@@ -87,11 +97,9 @@ pub fn dn_escape<'a, S: Into<Cow<'a, str>>>(lit: S) -> Cow<'a, str> {
         }
     }
     if let Some(output) = output {
-        // unchecked conversion is safe here: we receive a valid
-        // UTF-8 value, by definition, and only replace single ASCII
-        // bytes with ASCII byte sequences
+        // see the rationale for the same construct in ldap_escape()
         Cow::Owned(unsafe { String::from_utf8_unchecked(output) })
     } else {
-        lit.into()
+        val.into()
     }
 }
