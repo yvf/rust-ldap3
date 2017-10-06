4a905317940d8800689cae6133f84dd53e708252
Ivan Nejgebauer <inejge@gmail.com>
2017-10-06 12:16:20 +0200

    Clippy run

--- src/exop_impl/starttls.rs
+++ src/exop_impl/starttls.rs
@@ -7,6 +7,8 @@ pub const STARTTLS_OID: &'static str = "1.3.6.1.4.1.1466.20037";
 /// This operation isn't meant to be directly used by user code; it is used by
 /// connection-establishment routines when the StartTLS mechanism of securing the
 /// connection is requested by the user.
+// "StartTLS" is the form used in the RFCs; no need to change it or rename the struct
+#[cfg_attr(feature="cargo-clippy", allow(doc_markdown))]
 pub struct StartTLS;
 
 impl From<StartTLS> for Exop {
