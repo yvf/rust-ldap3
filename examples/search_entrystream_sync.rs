3d8eb2690fa3f38dc15da201b9f0cf676a3998e2
Ivan Nejgebauer <inejge@gmail.com>
2020-05-01 19:36:54 +0200

    Add synchronous Search example
    Plus technique for abandoning a search, plus demonstration of
    the prohibition of parallel use of EntryStream and LdapConn.

---
+++ examples/search_entrystream_sync.rs
@@ -0,0 +1,38 @@
+// Demonstrates:
+//
+// 1. Using a synchronous streaming Search.
+// 2. A commented-out invalid use of LdapConn while
+//    a derived EntryStream is active.
+// 3. The technique for abandoning the search, with
+//    directions for the ordering of steps to avoid
+//    double-borrowing.
+
+use ldap3::{LdapConn, Scope, SearchEntry};
+use ldap3::result::Result;
+
+fn main() -> Result<()> {
+    let mut ldap = LdapConn::new("ldap://localhost:2389")?;
+    let mut search = ldap
+        .streaming_search(
+            "ou=Places,dc=example,dc=org",
+            Scope::Subtree,
+            "(&(l=ma*)(objectClass=locality))",
+            vec!["l"],
+        )?;
+    while let Some(entry) = search.next()? {
+        let entry = SearchEntry::construct(entry);
+        println!("{:?}", entry);
+    }
+    // The following two statements show how one would
+    // Abandon a Search. The statements are commented out
+    // because the ldap handle shouldn't be used before result()
+    // is called on the streaming hanlde. To work, a) abandon()
+    // should follow result(), b) there should be no error
+    // handling of result(), because a prematurely finished
+    // stream will always return an error.
+    //
+    //let msgid = search.last_id();
+    //ldap.abandon(msgid)?;
+    let _res = search.result().success()?;
+    Ok(ldap.unbind()?)
+}
