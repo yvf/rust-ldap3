866daa83eb66bc0ee418d8bb9da1fbf9b3e1285e
Ivan Nejgebauer <inejge@gmail.com>
2020-11-25 20:18:39 +0100

    Refactor Matched Values filter parsing
    * Make the MV filter parser separate from the general Search filter
      parser to avoid accepting MV filters as valid by the latter.
    
    * Update Matched Values control documentation.

--- src/controls_impl/matched_values.rs
+++ src/controls_impl/matched_values.rs
@@ -3,13 +3,16 @@ use lber::{structures::ASNTag, write};
 
 use super::RawControl;
 
-use crate::filter::parse;
+use crate::filter::parse_matched_values;
 
 pub const MATCHED_VALUES_OID: &str = "1.2.826.0.1.3344810.2.3";
 
-/// Matched Results control ([RFC 3876](https://tools.ietf.org/html/rfc3876.html))
+/// Matched Values control ([RFC 3876](https://tools.ietf.org/html/rfc3876.html))
 ///
-/// This is used to ask the server to filters values of attribute
+/// This control is used to return a subset of attribute values from an entry.
+/// To avoid complicating the filter parser, the `dnAttributes` flag can be set
+/// in an Extensible Match expression despite not being specified in the RFC and
+/// having no meaning in this kind of filter.
 #[derive(Clone, Debug)]
 pub struct MatchedValues<S> {
     filter: S,
@@ -26,7 +29,9 @@ impl<S: AsRef<str>> MatchedValues<S> {
 impl<S: AsRef<str>> From<MatchedValues<S>> for RawControl {
     fn from(assn: MatchedValues<S>) -> RawControl {
         let filter_ref = assn.filter.as_ref();
-        let filter = parse(filter_ref).expect("filter").into_structure();
+        let filter = parse_matched_values(filter_ref)
+            .expect("filter")
+            .into_structure();
         let mut buf = BytesMut::with_capacity(filter_ref.len()); // ballpark
         write::encode_into(&mut buf, filter).expect("encoded");
         RawControl {
